fc9444736de5c3278913f8482b5576a41de2dbf8
James Rawlings <rawlingsj80@gmail.com>
2020-02-12 22:33:15 +0000

    remove corba config as not used or needed yet

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -3,9 +3,7 @@ package root
 import (
 	"fmt"
 	"github.com/jenkins-x-labs/jx-experiments/cmd/helmboot"
-	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"os"
 )
 
@@ -23,9 +21,6 @@ var (
 Commands can mature to alpha and beta phases before being accepted into the core jx command
 Complete documentation is available at https://jenkins-x.io
 `,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-	},
 })
 
 func Execute() {
@@ -36,41 +31,5 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jx-experiments/.cobra.yaml)")
-	rootCmd.PersistentFlags().StringP("author", "a", "Jenkins X Authors", "author name for copyright attribution")
-	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
-	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-	viper.SetDefault("author", "Jenkins X Authors jenkins-x@googlegroups.com")
-	viper.SetDefault("license", "apache")
-
 	rootCmd.AddCommand(helmboot.Helmboot)
-
-}
-
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error getting home directory: %s", err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".jx-experiments")
-	}
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
