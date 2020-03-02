bc5d01a1ecada25f6aec45b3dbb6ed3a61540d0d
James Strachan <james.strachan@gmail.com>
2020-03-02 08:22:56 +0000

    fix: try use glcoud to build the image

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-if [ -z "$GCP_SA" ]
-then
-  echo "no GCP SA specified"
-else
-  echo "enabling GCP Service Account from $GCP_SA"
-  gcloud auth activate-service-account --key-file $GCP_SA
-fi
-
-
 echo "building container image version: $VERSION"
 
-gcloud builds submit --config cloudbuild.yaml --project jenkinsxio-labs --substitutions=_VERSION="$VERSION"
-
+gcloud builds submit --config cloudbuild.yaml --project jenkinsxio-labs
