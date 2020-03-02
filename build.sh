368e5a48a685b074d24c6c7bf14d93e938d1c1b8
James Strachan <james.strachan@gmail.com>
2020-03-02 08:56:01 +0000

    fix: lets add the GCP SA

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ -z "$GCP_SA" ]
+then
+  echo "enabling GCP Service Account from $GCP_SA"
+  gcloud auth activate-service-account --key-file $GCP_SA
+fi
+
+
 echo "building container image version: $VERSION"
 
 gcloud builds submit --config cloudbuild.yaml --project jenkinsxio-labs
