fa7fca9cf836ec525c7da3ee4542028d98dd1d11
James Strachan <james.strachan@gmail.com>
2020-03-06 11:30:23 +0000

    fix: upgrade helmboot and add promote steps

--- promote.sh
+++ promote.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo "promoting the new version ${VERSION} to downstream repositories"
+
+jx step create pr regex \
+    --regex '\s+image: gcr.io/jenkinsxio-labs/jxl:(.*)' \
+    --version ${VERSION} \
+    --files "jenkins-x-*.yml" \
+    --repo https://github.com/jenkins-x/jenkins-x-versions.git
+
+jx step create pr versions -k docker -n gcr.io/jenkinsxio-labs/jxl -v ${VERSION}
+jx step create pr versions -k packages -n jxl -v ${VERSION}
+
+jx step create pr regex \
+    --regex '\version: (.*)' \
+    --version ${VERSION} \
+    --files charts/jx-labs/jxl-boot.yml \
+    --repo https://github.com/jenkins-x/jenkins-x-versions.git
