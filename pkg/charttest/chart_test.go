198a2a2190e51c7363096d712b5f6a9086adc07a
James Strachan <james.strachan@gmail.com>
2020-03-09 16:53:41 +0000

    fix: disable helm tests in CI/CD pipeline

--- pkg/charttest/chart_test.go
+++ pkg/charttest/chart_test.go
@@ -5,9 +5,21 @@ import (
 	"testing"
 
 	"github.com/jenkins-x/helm-unit-tester/pkg"
+	"github.com/jenkins-x/jx/pkg/util"
 )
 
 func TestChartsWithDifferentValues(t *testing.T) {
+	cmd := util.Command{
+		Name: "helm",
+		Args: []string{"version"},
+	}
+	helmVersion, err := cmd.RunWithoutRetry()
+	if err != nil {
+		t.Logf("helm not on the PATH so skipping: %s", helmVersion)
+		t.SkipNow()
+		return
+	}
+
 	chart := "../../charts/jxl-boot"
 
 	_, testcases := pkg.RunTests(t, chart, filepath.Join("test_data"))
